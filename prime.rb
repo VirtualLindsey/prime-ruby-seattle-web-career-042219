07ebe46201e57973a24629749319de48aa946752
L D <tapircode@gmail.com>
2019-04-10 04:02:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,14 +5,17 @@ def prime?(n)
   elsif n % 2 = 0 or % 3 = 0
       return false
   
- let j ← 1
+ j = 1
  while j * j ≤ n
     i = 6 * j + 1
     if n mod i = 0 or n mod (i + 2) = 0
         return false
+    end
+    
     i = 6 * j - 1
     if n mod i = 0 or n mod (i + 2) = 0
         return false
+    end
     j ← j + 1
  return true
 end

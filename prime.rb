f7db03d14979ff8edfa229804f37c12d11c230bb
L D <tapircode@gmail.com>
2019-04-10 04:03:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
   if n ≤ 3
     return n > 1
-  elsif n % 2 = 0 or % 3 = 0
+  elsif n % 2 == 0 or % 3 == 0
       return false
   
  j = 1
@@ -13,7 +13,7 @@ def prime?(n)
     end
     
     i = 6 * j - 1
-    if n mod i = 0 or n mod (i + 2) = 0
+    if n mod i == 0 or n mod (i + 2) == 0
         return false
     end
     j ← j + 1
